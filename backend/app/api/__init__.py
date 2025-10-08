00221f9d3dde3a5d0f45f82e145bd1e8bdd5dee8
SrinivasThrivv <srinivas@thrivv.ai>
2025-10-08 22:39:13 +0530

    Delete backend directory

--- backend/app/api/__init__.py
+++
@@ -1,7 +0,0 @@
-# app/api/__init__.py
-
-from fastapi import APIRouter
-from app.api.routes import register
-
-api_router = APIRouter()
-api_router.include_router(register.router, prefix="", tags=["Registration"])
--- backend/app/main.py
+++
@@ -1,7 +0,0 @@
-# app/main.py
-from fastapi import FastAPI
-from app.api import api_router
-
-app = FastAPI(title="Onboarding Agent API")
-
-app.include_router(api_router)
